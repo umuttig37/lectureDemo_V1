ffd4f3219b26387e6216eee51754626da5982f01
Umut Uygur <umut.uygur30@gmail.com>
2024-10-25 14:15:25 +0300

    dockerfile added

--- src/main/java/org/example/LocalizedGreeting.java
+++ src/main/java/org/example/LocalizedGreeting.java
@@ -12,6 +12,8 @@ public class LocalizedGreeting {
         System.out.println("1. English");
         System.out.println("2. French");
         System.out.println("3. Spanish");
+        System.out.println("4. Farsi");
+        System.out.println("5. German");
 
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
@@ -27,6 +29,12 @@ public class LocalizedGreeting {
                 break;
             case 3:
                 locale = new Locale("es", "ES");
+                break;
+            case 4:
+                locale = new Locale("fa", "IR");
+                break;
+            case 5:
+                locale = new Locale("de", "DE");
                 break;
             default:
                 System.out.println("Invalid choice. Defaulting to English.");
--- src/test/java/LocalizedGreetingTest.java
+++ src/test/java/LocalizedGreetingTest.java
@@ -30,6 +30,30 @@ public class LocalizedGreetingTest {
     }
 
     @Test
+    public void testEnglishLocale() {
+        Locale locale = new Locale("en", "US");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        assertEquals("Hello! Welcome to our application.", messages.getString("greeting"));
+    }
+    @Test
+    public void testSpanishLocale() {
+        Locale locale = new Locale("es", "ES");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        assertEquals("Hola! Bienvenido a nuestra aplicación.", messages.getString("greeting"));
+    }
+    @Test
+    public void testFrenchLocale() {
+        Locale locale = new Locale("fr", "FR");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        assertEquals("Bonjour! Bienvenue sur notre application.", messages.getString("greeting"));
+    }
+    @Test
+    public void testFarsiLocale() {
+        Locale locale = new Locale("fa", "IR");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        assertEquals("سلام! به برنامه ما خوش آمدید.", messages.getString("greeting"));
+    }
+    @Test
     public void testInvalidChoiceDefaultsToEnglish() {
         Locale locale = new Locale("en", "US"); // Simulate default choice
         ResourceBundle messages = ResourceBundle.getBundle("Messages", locale);
